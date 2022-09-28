f604e065daa0e43261cad33a6823e9379cd72d52
CJ <kubachod@gmail.com>
2022-09-28 17:37:27 +0200

    Initial Commit

---
+++ ForestSurvive/ForestSurviveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestSurviveApp.swift
+//  ForestSurvive
+//
+//  Created by Jakub Chodara on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForestSurviveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
