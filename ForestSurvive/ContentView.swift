7336375fd2f3e07f626d1088dc2df668ff24a385
CJ <kubachod@gmail.com>
2022-09-30 21:37:26 +0200

    DataBase Feature added

--- ForestSurvive/ContentView.swift
+++ ForestSurvive/ContentView.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
-
+import CoreData
 
 struct ContentView: View {
+    
+    
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    
+    private var items: FetchedResults<Item>
     
     @State private var showMenu: Bool = false
     @State  var long = 21.0
     @State  var lat = 53.0
     @State private var showWebView = false
     @State var MyPoints1 = Shared.shared.MyPoints
+    
+
     
     init(){
         let navBarApperance = UINavigationBarAppearance()
@@ -44,6 +55,7 @@ struct ContentView: View {
                                     .padding(.top)
                                 Button {
                                     MyPoints1 = Shared.shared.MyPoints
+                                    addItem()
                                 } label: {
                                     Image(systemName: "arrow.clockwise")
                                         .resizable()
@@ -114,6 +126,32 @@ struct ContentView: View {
                           
                         }
                     }
+                    
+                    Section(header: Text("DataBase")){
+                     
+                            List {
+                                ForEach(items) { item in
+                                    NavigationLink {
+                                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    } label: {
+                                        Text(item.timestamp!, formatter: itemFormatter)
+                                    }
+                                }
+                                .onDelete(perform: deleteItems)
+                            }
+                            .toolbar {
+                                ToolbarItem(placement: .navigationBarTrailing) {
+                                    //EditButton()
+                                }
+//                                ToolbarItem {
+//                                    Button(action: addItem) {
+//                                        Label("Add Item", systemImage: "plus")
+//                                    }
+//                                }
+                            }
+                            Text("Select an item")
+                        
+                    }
                 }
             
                 
@@ -152,10 +190,51 @@ struct ContentView: View {
         }
  
     }
+    
+    
+    
+    private func addItem() {
+        withAnimation {
+            let newItem = Item(context: viewContext)
+            newItem.timestamp = Date()
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
 }
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
